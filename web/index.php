ff1b3be7d3865660232e8899073a78cfcfd914a3
gaoyan <405704570@qq.com>
2014-10-24 18:07:08 +0800

    手机端增加 电脑版 链接和功能

--- web/index.php
+++ web/index.php
@@ -22,10 +22,9 @@ if ((DEBUG_MODE & 2) != 2)
     $smarty->caching = true;
 }
 $ua = strtolower($_SERVER['HTTP_USER_AGENT']);
-
 $uachar = "/(nokia|sony|ericsson|mot|samsung|sgh|lg|philips|panasonic|alcatel|lenovo|cldc|midp|mobile)/i";
-
-if(($ua == '' || preg_match($uachar, $ua))&& !strpos(strtolower($_SERVER['REQUEST_URI']),'wap'))
+$ispc = !empty($_GET['pc']) ? 1 : '';
+if(($ua == '' || preg_match($uachar, $ua))&& !strpos(strtolower($_SERVER['REQUEST_URI']),'wap') && !$ispc)
 {
     $Loaction = 'mobile/';
 
